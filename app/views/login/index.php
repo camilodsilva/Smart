3335249e8f9c4b9a3debb50cbb96b32a4d7642f3
CamiloDSilva <camilods_mp@hotmail.com>
2018-08-08 22:52:01 -0300

    Folder lib moved from project/app/lib to project/lib

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,6 +1,6 @@
 <p>
     Login<br />
-    <form action="?url=login/login" method="post">
+    <form action="login/login" method="post">
         <input type="text" placeholder="Email" name="user_email" id="user_email"><br />
         <input type="submit" value="Login">
     </form>
